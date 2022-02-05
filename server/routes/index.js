a181b1e3fdbb1f415a74bfd7d2aa71e25a673762
Ezequiel <ezequielamin@outlook.com>
2022-02-05 01:17:22 -0300

    ultima prueba

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+const publicPath = path.join(__dirname, "../public");
 
 router.get("*",(req,res)=>{
     res.sendFile(path.join(publicPath, 'index.html'))
